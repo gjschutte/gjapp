03584102087b2c8cefb43114a499a53f2e046928
gjschutte <schutte@debian.schutte>
2015-05-25 19:24:47 +0200

    Heroku 2

--- vul_spreuk.py
+++ vul_spreuk.py
@@ -6,6 +6,12 @@ db.session.commit()
 d = models.Domein(soort='Spreuk', omschrijving ='ICT')
 db.session.add(d)
 db.session.commit()
+d = models.Domein(soort='Spreuk', omschrijving ='Humor')
+db.session.add(d)
+db.session.commit()
+d = models.Domein(soort='Spreuk', omschrijving ='Overig')
+db.session.add(d)
+db.session.commit()
 
 
 domeinen = models.Domein.query.all()
