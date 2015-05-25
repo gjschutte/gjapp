a7d36351bf8bd62274b13e2d40c334f0abb5a892
gjschutte <schutte@debian.schutte>
2015-05-25 16:14:05 +0200

    heroku 1

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'you-will-never-guess'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATABASSE_URL') is None:
+if os.environ.get('DATABASE_URL') is None:
 	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'gjapp.db')
 else:
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
