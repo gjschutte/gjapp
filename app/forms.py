9c624cbf23752b0af2c72a449e7ec655bb536019
gjschutte <schutte@debian.schutte>
2015-05-30 13:10:43 +0200

    Domein toegevoegd

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,15 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField
+from wtforms.ext.sqlalchemy.orm import QuerySelectField
 from wtforms.validators import DataRequired, Length
+from .models import Domein
+
+def domeinen():
+	return Domein.query.filter_by(soort='Spreuk').order_by(Domein.omschrijving).all()
 
 class SpreukForm(Form):
 	body = TextAreaField('body', validators=[Length(min=0, max=140)])
 	auteur = StringField('auteur')
+	
+	soort = QuerySelectField('soort', query_factory=domeinen, get_label="omschrijving", allow_blank=False)
+
--- app/views.py
+++ app/views.py
@@ -2,11 +2,13 @@ from flask import render_template, redirect, session, url_for
 from app import app, db
 from .forms import SpreukForm
 from .models import Spreuk, Domein
+import random
 
 @app.route('/')
 @app.route('/index')
 def index():
-	spreuk = Spreuk.query.first()
+	rand = random.randrange(0, Spreuk.query.count())
+	spreuk = Spreuk.query[rand]
 	
 	return render_template('index.html',
 							title='Home',
@@ -17,7 +19,7 @@ def beheer():
 	form = SpreukForm()
 	if form.validate_on_submit():
 		d = Domein.query.get(2)
-		spreuk = Spreuk(body=form.body.data, auteur=form.auteur.data, domain=d)
+		spreuk = Spreuk(body=form.body.data, auteur=form.auteur.data, domain=form.soort.data)
 		db.session.add(spreuk)
 		db.session.commit()
 		return redirect(url_for('beheer'))
