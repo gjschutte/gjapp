00a96af756694d47428b89f1da7df77dbba3aa3c
gjschutte <schutte@debian.schutte>
2015-06-06 20:06:21 +0200

    Analyse toegevoegd

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, redirect, session, url_for
+from flask import render_template, redirect, session, url_for, request
 from app import app, db
 from .forms import SpreukForm
 from .models import Spreuk, Domein
 import random
+from ua_parser import user_agent_parser
+from user_agents import parse
 
 @app.route('/')
 @app.route('/index')
@@ -25,3 +27,14 @@ def beheer():
 		return redirect(url_for('beheer'))
 	spreuken = Spreuk.query.all()
 	return render_template('beheer.html', form=form, spreuken=spreuken)
+
+@app.route('/analyse')
+def analyse():
+	# parse the user agent
+	# user_agent_string = request.META.get('HTTP_USER_AGENT')
+	user_agent_string = request.headers.get('User-Agent')
+	user_agent = parse(user_agent_string)
+	
+	return render_template('analyse.html',
+							title='Gegevens computer',
+							user_agent = user_agent)
