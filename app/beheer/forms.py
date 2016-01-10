02efa23dbd159c0ec64d917d3bfd1913dbd57e78
gjschutte <schutte@debian.schutte>
2016-01-10 21:59:51 +0100

    Bootstrap added

--- app/beheer/forms.py
+++ app/beheer/forms.py
@@ -1,15 +1,16 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.ext.sqlalchemy.orm import QuerySelectField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import Required, Length
 from app.models import Domein
 
+
 def domeinen():
-	return Domein.query.filter_by(soort='Spreuk').order_by(Domein.omschrijving).all()
+    return Domein.query.filter_by(soort='Spreuk').order_by(Domein.omschrijving).all()
+
 
 class SpreukForm(Form):
-	body = TextAreaField('body', validators=[Length(min=0, max=140)])
-	auteur = StringField('auteur')
-	
-	soort = QuerySelectField('soort', query_factory=domeinen, get_label="omschrijving", allow_blank=False)
-
+    body = TextAreaField('Quote', validators=[Length(min=1, max=250, message=
+    (u'Quote should be filled and no longer than 250 characters'))])
+    auteur = StringField('Author', validators=[Required('Please provide an author')])
+    soort = QuerySelectField('Category', query_factory=domeinen, get_label="omschrijving", allow_blank=False)
--- app/weather/place_form.py
+++ app/weather/place_form.py
@@ -1,9 +1,9 @@
 from flask.ext.wtf import Form
 from wtforms import StringField
-from wtforms.validators import Length
+from wtforms.validators import Required
 
 class PlaceForm(Form):
-	City = StringField('City', validators=[Length(min=2, max=100)])
+	City = StringField('City', validators=[Required ('Please provide  a city')])
 	
 
 
